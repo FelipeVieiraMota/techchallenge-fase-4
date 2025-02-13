f415d8ed61f88f50ff934d5d3178f85dceee6b3f
Lucas Alves <lcalves012@gmail.com>
2025-02-13 12:31:32 -0300

    Added new microservice client_microservice in the project

--- compose.sh
+++ compose.sh
@@ -1,6 +1,6 @@
 clear
 #docker-compose up
 docker-compose down
-docker rm -f order-management-app-container delivery-logistic-app-container product-catalog-app-container
-docker rmi order-management-app-image delivery-logistic-app-image product-catalog-app-image
+docker rm -f order-management-app-container delivery-logistic-app-container product-catalog-app-container client-app-container
+docker rmi order-management-app-image delivery-logistic-app-image product-catalog-app-image client-app-image
 docker-compose up --build
