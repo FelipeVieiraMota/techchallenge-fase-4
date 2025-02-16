257fbceb8a3ecd3eb47c5cccccff8f6c643917cb
Lucas Alves <lcalves012@gmail.com>
2025-02-15 21:35:00 -0300

    Adicionando Gateway.

---
+++ delivery-logistic-microservice/src/main/java/com/motavieirafelipe/delivery_logistic_microservice/controller/consumer/OrderConsumer.java
@@ -0,0 +1,21 @@
+package com.motavieirafelipe.delivery_logistic_microservice.controller.consumer;
+
+import com.motavieirafelipe.delivery_logistic_microservice.events.OrderCreatedEvent;
+import com.motavieirafelipe.delivery_logistic_microservice.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Service
+@RequiredArgsConstructor
+public class OrderConsumer {
+
+    private final DeliveryService deliveryService;
+
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreated() {
+        return deliveryService::processEventMessage;
+    }
+}
